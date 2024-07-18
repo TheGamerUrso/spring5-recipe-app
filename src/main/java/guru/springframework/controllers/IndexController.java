121924123f111000f2d5cc108925c93a05f4bc7e
Petros Kitsaras <petros.kitsaras@gmail.com>
2024-07-18 13:26:56 +0200

    Added IndexController Tests

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -18,7 +18,7 @@ public class IndexController
     }
 
     @RequestMapping({"","/","/index"})
-    public String requestMethodName(Model model) {
+    public String getIndexPage(Model model) {
         log.debug("Getting Index page");
         model.addAttribute("recipe",recipeService.getRecipes());
         return "index";
